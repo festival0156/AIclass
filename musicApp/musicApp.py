fbf63021534e6771198788188ab202e2ba526cd3
festival0156 <71260920+festival0156@users.noreply.github.com>
2020-12-24 13:48:24 +0530

    Update musicApp.py

--- musicApp/musicApp.py
+++ musicApp/musicApp.py
@@ -1,4 +1,26 @@
 import tkinter as tk
+from tkinter import font
+style = {
+    'bgColor': '#121212',
+    'fgColor': 'white',
+    'title': 'Music App',
+    'buttonFont': {
+        'family': 'tahoma',
+        'size': 20
+    }
+}
+
 root = tk.Tk()
-lbl = Label(root, text='Loading configuration ...')
-lbl.pack()
+root.configure(bg = style['bgColor'])
+root.title(style['title'])
+root.geometry('1000x550')
+root.resizable(False, False)
+
+def drawOpenScreen():
+    opnBtn = tk.Button(root, text = 'Open File(s)', bg = style['bgColor'], fg = style['fgColor'],
+                       font = font.Font(family = style['buttonFont']['family'], size = style['buttonFont']['size']),
+                       padx = 90, pady = 20)
+    opnBtn.place(anchor = 'center', relx = 0.5, rely = 0.5)
+
+drawOpenScreen()
+root.mainloop()
