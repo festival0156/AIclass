ec4b7ae2abd8114c4a3b2f945c7f67538f1ae224
festival0156 <71260920+festival0156@users.noreply.github.com>
2020-12-24 23:15:05 +0530

    Update musicApp.py

--- musicApp/musicApp.py
+++ musicApp/musicApp.py
@@ -1,20 +1,28 @@
 import tkinter as tk
 from tkinter import font
 style = {
-    'bgColor': '#121212',
-    'fgColor': 'white',
-    'root': {
-        'title': 'Music App',
-        'geometry': '1000x550' # size of window
+  'bgColor': '#121212',
+  'fgColor': 'white',
+  'root': {
+    'title': 'Music App',
+    'geometry': '1200x400' # size of window
+  },
+  'openScreen': { # opening screen
+    'openButton': {
+      'text': 'Open File(s)',
+      'font': lambda: font.Font(family = 'tahoma', size = 20),
+      'padx': 100,
+      'pady': 20
     },
-    'openScreen': { # opening screen
-        'openButton': {
-            'text': 'Open File(s)',
-            'font': lambda: font.Font(family = 'tahoma', size = 20),
-            'padx': 100,
-            'pady': 20
-        }
+    'filedialog': {
+      'title': 'Open ...'
     }
+  },
+  'loadingScreen': {
+    'label': {
+      'font': lambda: font.Font(family = 'times new roman', size = 20),
+    }
+  }
 }
 
 root = tk.Tk()
@@ -23,21 +31,22 @@ root.title(style['root']['title'])
 root.geometry(style['root']['geometry'])
 root.resizable(False, False)
 
-class OpenScreen:
+class OpeningScreen:
     def draw(this):
         this.opnBtn = tk.Button(root, text = style['openScreen']['openButton']['text'], bg = style['bgColor'], fg = style['fgColor'],
                                 font = style['openScreen']['openButton']['font'](), padx = style['openScreen']['openButton']['padx'],
-                                pady = style['openScreen']['openButton']['pady'], command = OpenScreen.cmd)
+                                pady = style['openScreen']['openButton']['pady'], command = OpeningScreen.cmd)
         this.opnBtn.place(anchor = 'center', relx = 0.5, rely = 0.5)
     def destroy(this):
         this.opnBtn.destroy()
     def cmd(): # executed when open button pressed
         from tkinter import filedialog
-        fs = filedialog.askopenfilenames(parent = root, title = 'Open ...')
+        fs = filedialog.askopenfilenames(parent = root, title = style['openScreen']['filedialog']['title'])
         global opnScrn, playScrn, player
         opnScrn.destroy()
         player = Player(fs)
         playScrn = PlayScreen(player)
+        playScrn.draw()
 
 class PlayScreen:
     def __init__(this, player):
@@ -48,22 +57,41 @@ class PlayScreen:
     def destroy():
         pass
 
+class ErrorScreen:
+    def draw(this):
+        pass
+    def destroy(this):
+        pass
+
 class Player:
     def __init__(this, playlist):
         this.playlist = list(playlist)
-        this.i = 0
+        from pygame import mixer
+        mixer.init()
+        this.nowplaying = 0
+        mixer.music.load(this.playlist[0])
+        mixer.music.play()
+        this.music = mixer.music
     def play(this):
-        pass
+        this.music.unpause()
+    def pause(this):
+        this.music.pause()
     def previous(this):
-        pass
+        this.nowplaying -= 1
+        this.music.unload()
+        this.music.load(this.playlist[this.nowplaying])
+        this.music.play()
     def next(this):
-        pass
-    def volume(this, increaseBy):
-        pass
+        this.nowplaying += 1
+        this.music.unload()
+        this.music.load(this.playlist[this.nowplaying])
+        this.music.play()
+    def volume(this, set2):
+        this.set_volume(set2)
     def seek(this, time):
         pass
 
-opnScrn = OpenScreen()
+opnScrn = OpeningScreen()
 plyScrn = None
 player = None
 opnScrn.draw()
