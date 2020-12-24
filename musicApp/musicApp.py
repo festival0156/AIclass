69a868f817bcf100c8955e15dc6dcff5f9c39e94
festival0156 <71260920+festival0156@users.noreply.github.com>
2020-12-24 14:43:12 +0530

    Update musicApp.py

--- musicApp/musicApp.py
+++ musicApp/musicApp.py
@@ -3,14 +3,24 @@ from tkinter import font
 style = {
     'bgColor': '#121212',
     'fgColor': 'white',
-    'title': 'Music App',
-    'buttonFont': lambda: font.Font(family = 'tahoma', size = 20)
+    'root': {
+        'title': 'Music App',
+        'geometry': '1000x550'
+    }
+    'openScreen': {
+        'openButton': {
+            'text': 'Open File(s)',
+            'font': lambda: font.Font(family = 'tahoma', size = 20),
+            'padx': 100,
+            'pady': 20
+        }
+    }
 }
 
 root = tk.Tk()
 root.configure(bg = style['bgColor'])
-root.title(style['title'])
-root.geometry('1000x550')
+root.title(style['root']['title'])
+root.geometry(style['root']['geometry'])
 root.resizable(False, False)
 
 class OpenScreen:
@@ -18,11 +28,13 @@ class OpenScreen:
         from tkinter import filedialog
         files = filedialog.askopenfilenames(mode = 'rb', parent = root, title = 'Open ...')
     def draw(this):
-        this.opnBtn = tk.Button(root, text = 'Open File(s)', bg = style['bgColor'], fg = style['fgColor'],
-                                font = style['buttonFont'](), padx = 90, pady = 20)
+        this.opnBtn = tk.Button(root, text = style['openScreen']['openButton']['text'], bg = style['bgColor'], fg = style['fgColor'],
+                                font = style['openScreen']['openButton']['font'](), padx = style['openScreen']['openButton']['padx'],
+                                pady = style['openScreen']['openButton']['pady'], command = None)
         this.opnBtn.place(anchor = 'center', relx = 0.5, rely = 0.5)
     def destroy(this):
         this.opnBtn.destroy()
 
 opnScrn = OpenScreen()
+opnScrn.draw()
 root.mainloop()
