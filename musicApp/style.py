051426de9b2fb4abcf29aa7db1f15c2e4de1998f
festival0156 <71260920+festival0156@users.noreply.github.com>
2020-12-29 15:37:07 +0530

    Update style.py

--- musicApp/style.py
+++ musicApp/style.py
@@ -9,12 +9,39 @@ style = {
     'openScreen': { # opening screen
         'openButton': {
             'text': 'Open File(s)',
-            'font': lambda: font.Font(family = 'tahoma', size = 20),
-            'padx': 100,
-            'pady': 20
+            'font': lambda: font.Font(family = 'tahoma', size = 23),
+            'padx': 160,
+            'pady': 17
         },
         'filedialog': {
             'title': 'Open ...'
+        }
+    },
+    'messages': {
+        'pygameError': {
+            'title': '',
+            'message':
+'''This environment does not meet the minimum requirements to run this program.
+
+Minumum System Requirements:
+    Python 3
+    Tkinter 8.6
+    Pygame 1.9
+
+Recommended System Requirements:
+    Python 3
+    Tkinter 8.6
+    Pygame 2.0
+    CPU: Core i3 2.4 GHz
+    GPU: AMD Rodeon HD 7870'''
+        },
+        'pygameWarning': {
+            'title': 'Version Warning!!',
+            'message': f'You are using pygame version {__import__("pygame").version.ver}; however, you should consider upgrading to at least version 2'
+        },
+        'python2warning': {
+        	'title': 'Version Warning!!',
+        	'message': f'You are using python version {__import__("sys").version}; however, you should consider upgrading to at least python 3'
         }
     },
     'playScreen': {
